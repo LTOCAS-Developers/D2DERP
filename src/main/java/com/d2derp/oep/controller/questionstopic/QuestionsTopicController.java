ae343859810b7cbe24dde7c5fd2afd33a367f793
balachander28 <54208802+balachander28@users.noreply.github.com>
2019-09-27 14:20:20 +0530

    changes in questionsTopic

--- src/main/java/com/d2derp/oep/controller/questionstopic/QuestionsTopicController.java
+++ src/main/java/com/d2derp/oep/controller/questionstopic/QuestionsTopicController.java
@@ -19,7 +19,7 @@ import com.d2derp.oep.pojo.UserPojo;
 import com.d2derp.oep.service.questionstopic.QuestionsTopicService;
 
 @RestController
-@RequestMapping("/questionsTopics")
+@RequestMapping("/questionsTopic")
 public class QuestionsTopicController {
 
 	@Autowired
