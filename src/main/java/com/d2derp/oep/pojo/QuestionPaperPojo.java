14dbd1046506a9833c926e699802e78ecb25dc12
vignesh <vigneshshank96@gmail.com>
2019-10-01 13:31:40 +0530

    questionPaper Commited by Vignesh

---
+++ src/main/java/com/d2derp/oep/pojo/QuestionPaperPojo.java
@@ -0,0 +1,22 @@
+package com.d2derp.oep.pojo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class QuestionPaperPojo {
+
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
